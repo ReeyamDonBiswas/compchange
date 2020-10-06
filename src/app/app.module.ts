4c30cf5d831cb2643f7d29c4dc60a2c7833a41fa
reeyam biswas <reeyambiswas@gmail.com>
2020-10-06 17:30:11 +0530

    tr commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserComponent } from './user/user.component';
+import { Parent1Component } from './parent1/parent1.component';
+import { Parent2Component } from './parent2/parent2.component';
+import { Parent3Component } from './parent3/parent3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserComponent
+    UserComponent,
+    Parent1Component,
+    Parent2Component,
+    Parent3Component
   ],
   imports: [
     BrowserModule,
